726517c256459e2c99f18ed727273a03a9f48f94
kyeongeun <jke@peopulley.com>
2022-03-18 17:39:36 +0900

    useful of onChange event, firebase setting

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChange = (event) => {
+        const {target: {name, value}} = event;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+        console.log(event.target.name);
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div>
-            <span>Auth</span>
+            <form onSubmit={onSubmit}>
+                <input type={"text"} name={"email"} onChange={onChange} placeholder={"Email"} value={email} required/>
+                <input type={"password"} name={"password"} onChange={onChange} placeholder={"Password"} value={password} required/>
+                <input type={"submit"} value={"Log In"} />
+            </form>
+            <button>Continue with Google</button>
+            <button>Continue with Github</button>
         </div>
     );
 };
